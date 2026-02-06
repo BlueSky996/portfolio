f45c514fb7bd29b654008bcae16d195ab6a4cbaa
ksheb <zexszexs2@gmail.com>
2026-02-06 02:01:05 -0600

    add hero section along with backgroundvideo

---
+++ src/components/BackgroundVideo.vue
@@ -0,0 +1,28 @@
+<template>
+    <video
+        class="bg-video"
+        autoplay
+        muted
+        loop
+        playsinline
+    >
+        <source src="/videos/background.mp4" type="video/mp4" />
+     </video>
+</template>
+
+<script>
+export default {
+    name: "BackgroundVideo",
+};
+</script>
+
+<style scoped>
+.bg-video {
+    position: fixed;
+    inset: 0;
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+    z-index: -1;
+}
+</style>
---
+++ src/sections/Hero.vue
@@ -0,0 +1,44 @@
+<template>
+    <section class="hero">
+        <div class="hero-left">
+            <h1>Mohamed Khasheebah</h1>
+            <p>
+                Web3 Full-stack dev
+            </p>
+
+            <div class="stack-slider">
+            stack slider ( will do later)
+            </div>
+        </div>
+
+        <div class="hero-right">
+            <img src="/images/me.png" alt="Me" />
+        </div>
+    </section>
+</template>
+
+<script>
+export default {
+    name: "Hero",
+};
+</script>
+
+<style scoped>
+.hero {
+    min-height: 100hv;
+    display: grid;
+    grid-template-columns: 1.2fr 1fr;
+    align-item: center;
+    padding: 4rem;
+    color: white;
+}
+
+.hero-left {
+    max-width: 600px;
+}
+
+.hero-right img {
+    width: 100%;
+    max-width: 420px;
+}
+</style>
