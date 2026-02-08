4a76cf5c47fda7a477b627c6b24bb5c416d56de6
ksheb <zexszexs2@gmail.com>
2026-02-07 23:06:59 -0600

    Updated app.vue to include projectgrid

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <BackgroundVideo />
   <Hero />
-  <div id="app">
-    Portoflio Loading ...
-    </div>
+  <ProjectsGrid />
 </template>
 
 <script>
 import BackgroundVideo from './components/BackgroundVideo.vue';
+import ProjectsGrid from './components/ProjectsGrid.vue';
 import Hero from "./sections/Hero.vue";
 
 export default {
-  components: { BackgroundVideo },
+  components: { BackgroundVideo, Hero, ProjectsGrid },
 };
 </script>
 
